3cbb0adeb481963b01e5f8d01f9e794f0a541efb
YZK Win <beaock@gmail.com>
2019-01-02 22:45:36 +0900

    Docker runserver的shell脚本(目前无法通过脚本启动flower)

--- docker/start-server.sh
+++ docker/start-server.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+echo "※※※==============正在启动Django服务器======================※※※"
+sleep 5
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
+
+echo "※※※==============正在启动队列任务管理后台======================※※※"
+sleep 5
+celery flower --broker=redis://redis:6379/1
+
+echo "※※※==============部署已完成,谢谢======================※※※"
